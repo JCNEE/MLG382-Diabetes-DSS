791fc3e9dc3450319b19e1d2ce38b4b87d9d9b7d
Mcyespitlick <mbcheyne@gmail.com>
2026-04-13 15:31:54 +0200

    Comment fix
    Added more comments to clarify the code sections

--- src/prepare_data.py
+++ src/prepare_data.py
@@ -7,13 +7,19 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 
 
+#==============================================================
+# 1.Setup paths
+#==============================================================
+
 script_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(script_dir, "../data/Diabetes_and_LifeStyle_Dataset_.csv")
 train_data_path = os.path.join(script_dir, "../data/train.csv")
 test_data_path = os.path.join(script_dir, "../data/test.csv")
 
 
-
+#==============================================================
+# 2. Fucntion to split the data into train and test sets
+#==============================================================
 
 def load_and_split(test_size=0.2, random_state=42):
     from sklearn.model_selection import train_test_split
